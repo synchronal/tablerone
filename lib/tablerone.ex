940367e569460a006254244ede1e42b3e7d51103
Eric Saxby <sax@livinginthepast.org>
2023-09-30 09:47:01 -0700

    Tablerone.icon loads dasherized icon files

--- lib/tablerone.ex
+++ lib/tablerone.ex
@@ -14,13 +14,21 @@ defmodule Tablerone do
   with instructions on how to run a mix task to download the icon.
   """
   defmacro icon(name) do
-    svg_path = Path.join([@priv_dir, "tablerone", "#{name}.svg"])
+    svg_name = Tablerone.dasherize(name)
+    svg_path = Path.join([@priv_dir, "tablerone", "#{svg_name}.svg"])
 
     svg_contents =
       if File.exists?(svg_path) do
         File.read!(svg_path)
       else
-        :elixir_errors.erl_warn(__ENV__.line, __ENV__.file, "Icon :#{name} has not been downloaded.")
+        :elixir_errors.erl_warn(__ENV__.line, __ENV__.file, """
+        Icon :#{name} has not been downloaded.
+
+        To download this icon to the local application, run the following in a terminal:
+
+            mix tablerone.download #{svg_name}
+        """)
+
         nil
       end
 
@@ -28,4 +36,7 @@ defmodule Tablerone do
       unquote(svg_contents)
     end
   end
+
+  def dasherize(icon_name) when is_atom(icon_name), do: dasherize(Atom.to_string(icon_name))
+  def dasherize(icon_name) when is_binary(icon_name), do: String.replace(icon_name, "_", "-")
 end
