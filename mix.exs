bb2d5e38676ab88d1e3466309e54e4b476eb8fa9
Eric Saxby <sax@livinginthepast.org>
2023-10-06 11:29:17 -0700

    Add package metadata to mix.exs
    Co-authored-by: Erik Hanson <erik@reflective.dev>

--- mix.exs
+++ mix.exs
@@ -1,17 +1,22 @@
 defmodule Tablerone.MixProject do
   use Mix.Project
 
+  @scm_url "https://github.com/synchronal/tablerone"
   @version "0.1.0"
 
   def project do
     [
       app: :tablerone,
       deps: deps(),
+      description: "Download load tabler icons to and load from the current project",
       dialyzer: dialyzer(),
       docs: docs(),
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
+      homepage_url: @scm_url,
+      package: package(),
       preferred_cli_env: [credo: :test, dialyzer: :test],
+      source_url: @scm_url,
       start_permanent: Mix.env() == :prod,
       version: @version
     ]
@@ -48,4 +53,11 @@ defmodule Tablerone.MixProject do
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp package,
+    do: [
+      licenses: ["MIT"],
+      maintainers: ["synchronal.dev", "Erik Hanson", "Eric Saxby"],
+      links: %{"GitHub" => @scm_url}
+    ]
 end
