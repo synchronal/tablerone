0510df4c9a08471ee299ca2c75de096d09c9db99
Eric Saxby <sax@livinginthepast.org>
2023-10-06 11:12:52 -0700

    Tablerone.icon is a function rather than a macro
    Co-authored-by: Erik Hanson <erik@reflective.dev>

--- lib/tablerone.ex
+++ lib/tablerone.ex
@@ -5,35 +5,27 @@ defmodule Tablerone do
   Documentation for `Tablerone`.
   """
 
-  @otp_app Application.compile_env(:tablerone, :otp_app)
-  @priv_dir :code.priv_dir(@otp_app)
+  @otp_app Application.compile_env!(:tablerone, :otp_app)
 
   @doc """
   Renders the given icon as a Phoenix component. If the icon has not been downloaded
-  to the priv directory of the parent application, `icon` will raise at compile time,
+  to the priv directory of the parent application, `icon` will raise at run time,
   with instructions on how to run a mix task to download the icon.
   """
-  defmacro icon(name) do
+  def icon(name) do
     svg_name = Tablerone.dasherize(name)
-    svg_path = Path.join([@priv_dir, "tablerone", "#{svg_name}.svg"])
+    svg_path = Path.join([:code.priv_dir(@otp_app), "tablerone", "#{svg_name}.svg"])
 
-    svg_contents =
-      if File.exists?(svg_path) do
-        File.read!(svg_path)
-      else
-        :elixir_errors.erl_warn(__ENV__.line, __ENV__.file, """
-        Icon :#{name} has not been downloaded.
+    if File.exists?(svg_path) do
+      File.read!(svg_path)
+    else
+      raise ArgumentError, """
+      Icon :#{name} has not been downloaded.
 
-        To download this icon to the local application, run the following in a terminal:
+      To download this icon to the local application, run the following in a terminal:
 
-            mix tablerone.download #{svg_name}
-        """)
-
-        nil
-      end
-
-    quote do
-      unquote(svg_contents)
+          mix tablerone.download #{svg_name}
+      """
     end
   end
 
